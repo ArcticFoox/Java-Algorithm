a2d6fc8e89217c1a9b4a98f02f4b964237e74edb
신종신 <jongsin16@naver.com>
2025-03-20 23:21:07 +0900

    1085
    basic

---
+++ src/Baekjoon/B1085/Main.java
@@ -0,0 +1,21 @@
+//1085 basic
+package Baekjoon.B1085;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.min(Math.min(Math.abs(x - w), Math.abs(y - h)), Math.min(x, y)));
+    }
+}
