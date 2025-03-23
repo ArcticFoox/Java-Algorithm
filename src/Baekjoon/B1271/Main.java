840a7a753e6c795105a13144aefba2118ab94f5b
신종신 <jongsin16@naver.com>
2025-03-23 22:02:37 +0900

    1271
    basic

---
+++ src/Baekjoon/B1271/Main.java
@@ -0,0 +1,21 @@
+//1271 basic
+package Baekjoon.B1271;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+
+        System.out.println(n/m);
+        System.out.println(n%m);
+
+    }
+}
