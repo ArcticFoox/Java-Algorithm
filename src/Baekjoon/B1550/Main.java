0374afe063c5ee1a883c63bf7ea83dfc2be0e868
신종신 <jongsin16@naver.com>
2025-03-30 21:49:54 +0900

    1550
    basic

---
+++ src/Baekjoon/B1550/Main.java
@@ -0,0 +1,14 @@
+//1550 basic
+package Baekjoon.B1550;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(Integer.parseInt(s, 16));
+    }
+}
