5f23098a9e8a3b5e5e4bc7b002b2711777e4ae45
ShinJongShin <jongsin16@naver.com>
2025-08-11 21:18:14 +0900

    15727
    basic

---
+++ src/Baekjoon/B15727/Main.java
@@ -0,0 +1,15 @@
+//15727 basic
+package Baekjoon.B15727;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println( N / 5 + (N % 5 == 0 ? 0 : 1) );
+    }
+}
