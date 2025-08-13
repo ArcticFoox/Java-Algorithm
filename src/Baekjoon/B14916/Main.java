8c2e98cad872ffd692a217eaacc699a9112cc31e
ShinJongShin <jongsin16@naver.com>
2025-08-13 21:40:42 +0900

    14916
    dp

---
+++ src/Baekjoon/B14916/Main.java
@@ -0,0 +1,32 @@
+//14916 dp
+package Baekjoon.B14916;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[100001];
+        arr[1] = -1;
+        arr[2] = 1;
+        arr[3] = -1;
+        arr[4] = 2;
+        arr[5] = 1;
+        for (int i = 6; i <= n; i++) {
+            if (arr[i - 2] == -1 && arr[i - 5] == -1) {
+                arr[i] = -1;
+            } else if (arr[i - 2] == -1) {
+                arr[i] = arr[i - 5] + 1;
+            } else if (arr[i - 5] == -1) {
+                arr[i] = arr[i - 2] + 1;
+            } else {
+                arr[i] = Math.min(arr[i - 2], arr[i - 5]) + 1;
+            }
+        }
+        System.out.println(arr[n]);
+    }
+}
