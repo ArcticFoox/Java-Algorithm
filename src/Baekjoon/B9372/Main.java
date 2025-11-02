059dcbc8daca53a53e1423291f4464bb906f175f
ShinJongShin <jongsin16@naver.com>
2025-11-02 18:39:32 +0900

    9372
    wtf

---
+++ src/Baekjoon/B9372/Main.java
@@ -0,0 +1,25 @@
+//9372 wtf
+package Baekjoon.B9372;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < M; j++) {
+                st = new StringTokenizer(br.readLine(), " ");
+            }
+            System.out.println(N - 1);
+        }
+    }
+}
