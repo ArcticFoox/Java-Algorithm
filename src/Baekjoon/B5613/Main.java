25242ea1bee5b5d0b9008e8fecdc3369946f9eef
ShinJongShin <jongsin16@naver.com>
2025-11-15 16:52:27 +0900

    5613
    basic
    https://www.acmicpc.net/problem/5613

---
+++ src/Baekjoon/B5613/Main.java
@@ -0,0 +1,33 @@
+//5613 basic
+package Baekjoon.B5613;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result = Integer.parseInt(br.readLine());
+
+        while(true) {
+            String S = br.readLine();
+
+            if(S.equals("=")) break;
+
+            int N = Integer.parseInt(br.readLine());
+
+
+            if(S.equals("+")) {
+                result += N;
+            }else if(S.equals("-")){
+                result -= N;
+            }else if(S.equals("*")){
+                result *= N;
+            }else{
+                result /= N;
+            }
+        }
+        System.out.println(result);
+    }
+}
