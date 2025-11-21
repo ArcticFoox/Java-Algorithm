426d82524f28cc1f7c1279a39c247df2dc3ad965
ShinJongShin <jongsin16@naver.com>
2025-11-21 20:34:23 +0900

    5339
    basic

---
+++ src/Baekjoon/B5339/Main.java
@@ -0,0 +1,17 @@
+//5339 basic
+package Baekjoon.B5339;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
