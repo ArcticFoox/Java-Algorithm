6adab888198ce81e473972b9de980a890910a640
ShinJongShin <jongsin16@naver.com>
2025-11-27 20:53:36 +0900

    15969
    basic

---
+++ src/Baekjoon/B15969/Main.java
@@ -0,0 +1,27 @@
+//15969 basic
+package Baekjoon.B15969;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[] scores = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            scores[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(scores);
+
+        System.out.println(scores[n - 1] - scores[0]);
+
+    }
+}
