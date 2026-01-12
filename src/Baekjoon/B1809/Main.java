91c21f27f8c1a694f5cec9cc18e9f4ca9948ab7f
ShinJongShin <jongsin16@naver.com>
2026-01-12 21:39:26 +0900

    1809
    ascii

---
+++ src/Baekjoon/B1809/Main.java
@@ -0,0 +1,8 @@
+//1809 ascii art
+package Baekjoon.B1809;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(("(___)\n(o o)____/\n @@      \\\n  \\ ____, /\n  //   //\n ^^   ^^"));
+    }
+}
