7d9ebe4bc7ff9989532fb40172c0d42314677ea1
ShinJongShin <jongsin16@naver.com>
2026-01-13 22:18:23 +0900

    11945
    string_reverse

---
+++ src/Baekjoon/B11945/Main.java
@@ -0,0 +1,31 @@
+//11945 string_reverse
+package Baekjoon.B11945;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        String[] result = new String[N];
+
+        for(int i = 0; i < N; i++) {
+            String str = br.readLine();
+            StringBuilder sb = new StringBuilder();
+            sb.append(str);
+            sb.reverse();
+            result[i] = sb.toString();
+        }
+
+        for(String s : result) {
+            System.out.println(s);
+        }
+    }
+}
