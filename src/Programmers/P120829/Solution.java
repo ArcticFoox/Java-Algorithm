9f7e4eaea2b5bc04ae5b0c787f534a55d2bfafe3
ShinJongShin <jongsin16@naver.com>
2026-01-25 22:51:01 +0900

    120829
    basic

---
+++ src/Programmers/P120829/Solution.java
@@ -0,0 +1,16 @@
+//120829 basic
+package Programmers.P120829;
+
+public class Solution {
+    public int solution(int angle) {
+        if(angle > 0 && angle < 90) {
+            return 1;
+        } else if(angle == 90) {
+            return 2;
+        } else if(angle > 90 && angle < 180) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
