d9cc887fd67b9c9366855b4b24e57a337089ee69
ShinJongShin <jongsin16@naver.com>
2026-01-30 23:11:55 +0900

    120820
    basic

---
+++ src/Programmers/P120820/Solution.java
@@ -0,0 +1,12 @@
+//120820 basic
+package Programmers.P120820;
+
+public class Solution {
+    public int solution(int age) {
+        int answer = 0;
+
+        answer = 2023 - age;
+
+        return answer;
+    }
+}
