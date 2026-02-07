667b8b081d8c4ab6f4aadc2ed819cb1ebc28d70d
ShinJongShin <jongsin16@naver.com>
2026-02-07 21:58:46 +0900

    120898
    basic

---
+++ src/Programmers/P120898/Solution.java
@@ -0,0 +1,9 @@
+//120898 basic
+package Programmers.P120898;
+
+public class Solution {
+    public int solution(String message) {
+        int answer = message.length() * 2;
+        return answer;
+    }
+}
